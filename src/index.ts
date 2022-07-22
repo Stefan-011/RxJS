d73ef703145af7a41237727ad16e03b1dfd7192c
Stefan-011 <stefan.stanimirovic000@gmail.com>
2022-07-22 20:14:37 +0200

    Pomeranje modela i dodatak HTML elemenata

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-alert("Radi!!");
+import {Drzava} from "./Models/Drzava"
+import {User} from "./Models/User"
+
+let Btns: HTMLButtonElement[] = []; // 0 - veca | 1 - manja 
+let Zastave: HTMLImageElement[] = []; // 0 - Leva | 1 - Desna
+let DrzavaNameLabel: HTMLLabelElement[] = []; // 0 - Leva | 1 - Desna
+let DrzavaPovLabel: HTMLLabelElement[] = []; // 0 - Leva | 1 - Desna
+let BrojPoena: HTMLLabelElement[] = []; // 0 - score | 1 - highscore
+let Drzave:Drzava[] = [];// 0 - Leva | 1 - Desna
+let Igrac:User;
