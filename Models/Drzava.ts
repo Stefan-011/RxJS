4c1b8b126d2e07118b2408eee33abbd3706132ac
Stefan-011 <stefan.stanimirovic000@gmail.com>
2022-07-22 18:03:09 +0200

    Models

---
+++ Models/Drzava.ts
@@ -0,0 +1,15 @@
+export class Drzava
+{
+    id:number;
+    ime:string;
+    zastava:string;
+    povrsina:number;
+
+    constructor()
+    {
+        this.id = 0;
+        this.ime = "";
+        this.zastava = "";
+        this.povrsina = 0;
+    }
+}
---
+++ Models/User.ts
@@ -0,0 +1,11 @@
+export class User
+{
+    score:number;
+    high_score:number;
+
+    constructor()
+    {
+        this.score = 0;
+        this.high_score = 0;
+    }
+}
