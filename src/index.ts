43f6435f5a10c2da6244a53720b6a0c1e5aee4ee
Stefan-011 <stefan.stanimirovic000@gmail.com>
2022-07-23 15:17:13 +0200

    Dodavanje html elemenata

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,23 @@ let DrzavaPovLabel: HTMLLabelElement[] = []; // 0 - Leva | 1 - Desna
 let BrojPoena: HTMLLabelElement[] = []; // 0 - score | 1 - highscore
 let Drzave:Drzava[] = [];// 0 - Leva | 1 - Desna
 let Igrac:User;
+
+window.onload =  function()
+{ 
+  DrzavaNameLabel[0] = document.getElementById("leva_drzava_ime") as HTMLLabelElement;
+  DrzavaNameLabel[1] = document.getElementById("desna_drzava_ime") as HTMLLabelElement;
+  
+  DrzavaPovLabel[0] = document.getElementById("leva_drzava_povrsina") as HTMLLabelElement;
+  DrzavaPovLabel[1] = document.getElementById("desna_drzava_povrsina") as HTMLLabelElement;
+  
+  Zastave[0] = document.getElementById("leva_drzava_zastava") as HTMLImageElement;
+  Zastave[1] = document.getElementById("desna_drzava_zastava") as HTMLImageElement;
+  
+  Btns[0] = document.getElementById("btn-veca") as HTMLButtonElement;
+  Btns[1] = document.getElementById("btn-manja") as HTMLButtonElement;
+
+  BrojPoena[0] = document.getElementById("br_poena") as HTMLLabelElement;
+  BrojPoena[1] = document.getElementById("max_poena") as HTMLLabelElement;
+
+
+}
