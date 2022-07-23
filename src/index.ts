942541c60b517bad3ae3a57f44cf7d2557d5c9e4
Stefan-011 <stefan.stanimirovic000@gmail.com>
2022-07-23 15:29:56 +0200

    Observables i environment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {Drzava} from "./Models/Drzava"
 import {User} from "./Models/User"
+import {GetDrzava} from "./Logic/observables"
 
 let Btns: HTMLButtonElement[] = []; // 0 - veca | 1 - manja 
 let Zastave: HTMLImageElement[] = []; // 0 - Leva | 1 - Desna
@@ -9,7 +10,7 @@ let BrojPoena: HTMLLabelElement[] = []; // 0 - score | 1 - highscore
 let Drzave:Drzava[] = [];// 0 - Leva | 1 - Desna
 let Igrac:User;
 
-window.onload =  function()
+window.onload =  async function()
 { 
   DrzavaNameLabel[0] = document.getElementById("leva_drzava_ime") as HTMLLabelElement;
   DrzavaNameLabel[1] = document.getElementById("desna_drzava_ime") as HTMLLabelElement;
